7a219ce96bbe7c79cdcb9900fab03cfbe86d3df4
Matthew Mongeau <halogenandtoast@gmail.com>
2021-11-16 17:42:29 -0600

    Cleanup

--- backend/marvel-core/library/Marvel/Support/Supports/Helicarrier.hs
+++ backend/marvel-core/library/Marvel/Support/Supports/Helicarrier.hs
@@ -4,17 +4,17 @@ module Marvel.Support.Supports.Helicarrier where
 import Marvel.Prelude
 
 import Marvel.Ability
-import Marvel.Support.Attrs
-import Marvel.Effect.Attrs
-import qualified Marvel.Support.Cards as Cards
 import Marvel.Card.Code
 import Marvel.Cost
 import Marvel.Criteria
+import Marvel.Effect.Attrs
 import Marvel.Entity
 import Marvel.Message
-import Marvel.Source
 import Marvel.Question
 import Marvel.Queue
+import Marvel.Source
+import Marvel.Support.Attrs
+import Marvel.Support.Cards qualified as Cards
 import Marvel.Target
 
 helicarrier :: SupportCard Helicarrier
@@ -36,6 +36,8 @@ newtype HelicarrierEffect = HelicarrierEffect EffectAttrs
 
 instance RunMessage HelicarrierEffect where
   runMessage msg e@(HelicarrierEffect attrs) = case msg of
-    IdentityMessage ident (PlayedCard _) | IdentityTarget ident == effectTarget attrs-> e <$ push (EffectMessage (toId attrs) DisableEffect)
+    IdentityMessage ident (PlayedCard _)
+      | IdentityTarget ident == effectTarget attrs -> e
+      <$ push (EffectMessage (toId attrs) DisableEffect)
     EndPhase _ -> e <$ push (EffectMessage (toId attrs) DisableEffect)
     _ -> HelicarrierEffect <$> runMessage msg attrs
