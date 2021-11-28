663c976abdf3c723ca2912874c128627a5442f68
Goose <halogenandtoast@gmail.com>
2021-11-28 12:43:18 -0600

    Ritual Combat

--- backend/marvel-core/library/Marvel/Treachery/Treacheries/RitualCombat.hs
+++ backend/marvel-core/library/Marvel/Treachery/Treacheries/RitualCombat.hs
@@ -5,14 +5,17 @@ module Marvel.Treachery.Treacheries.RitualCombat
 
 import Marvel.Prelude
 
-import Marvel.Card.Code
+import Marvel.Card
 import Marvel.Entity
+import Marvel.Id
+import Marvel.Matchers
 import Marvel.Message
+import Marvel.Question
 import Marvel.Queue
 import Marvel.Source
 import Marvel.Target
 import Marvel.Treachery.Attrs
-import Marvel.Treachery.Cards qualified as Cards
+import qualified Marvel.Treachery.Cards as Cards
 
 ritualCombat :: TreacheryCard RitualCombat
 ritualCombat = treachery RitualCombat Cards.ritualCombat
@@ -29,6 +32,23 @@ instance RunMessage RitualCombat where
           push $ DiscardTopOfEncounterDeck 1 (Just $ toTarget attrs)
           pure . RitualCombat $ attrs & resolverL ?~ ident
         _ -> RitualCombat <$> runMessage msg attrs
-    WithDiscarded target _ cards | isTarget attrs target -> do
-      pure t
+    WithDiscarded target _ (onlyEncounterCards -> cards)
+      | isTarget attrs target -> do
+        case treacheryResolver attrs of
+          Just ident -> do
+            let
+              x = (+ 1) . fromIntegral . sum $ map
+                (length . cdBoostIcons . getCardDef)
+                cards
+            chooseOne
+              ident
+              [ Label
+                ("Deal " <> tshow x <> " damage to your hero")
+                [DamageCharacter (IdentityCharacter ident) (toSource attrs) x]
+              , Label
+                ("Place " <> tshow x <> " threat on the main scheme")
+                [PlaceThreat (toSource attrs) x MainScheme]
+              ]
+            pure t
+          Nothing -> error "no ident set"
     _ -> RitualCombat <$> runMessage msg attrs
