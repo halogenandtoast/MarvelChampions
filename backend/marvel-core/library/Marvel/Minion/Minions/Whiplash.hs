8265e62f1582efbed5f58c55a39655b68c1a191f
Matthew Mongeau <halogenandtoast@gmail.com>
2021-12-12 15:20:59 -0600

    Fix minion

--- backend/marvel-core/library/Marvel/Minion/Minions/Whiplash.hs
+++ backend/marvel-core/library/Marvel/Minion/Minions/Whiplash.hs
@@ -12,15 +12,12 @@ import Marvel.Hp
 import Marvel.Message
 import Marvel.Minion.Attrs
 import Marvel.Minion.Cards qualified as Cards
-import Marvel.Source
-import Marvel.Stats
-import Marvel.Target
 
 whiplash :: MinionCard Whiplash
 whiplash = minion Whiplash Cards.whiplash (Sch 2) (Atk 3) (HP 4)
 
 newtype Whiplash = Whiplash MinionAttrs
-  deriving anyclass IsMinion
+  deriving anyclass (IsMinion, HasModifiersFor)
   deriving newtype (Show, Eq, ToJSON, FromJSON, HasCardCode, Entity, IsSource, IsTarget)
 
 instance RunMessage Whiplash where
